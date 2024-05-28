b114adb99ab1508580e243a6f8411ba53840f0ae
Swati jagtap <swatijagtap5060@gmail.com>
2024-05-28 21:25:52 +0530

    submit

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+  res.end("Hello, world!")
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log(`Server started and listening on port ${port}`);
 });
 
 // DO NOT EDIT BELOW THIS LINE
